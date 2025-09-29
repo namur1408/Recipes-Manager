0f2357703a0ae34c8869e17170aacf68c340a6db
oleg_androsiuk <a.oleg1408@gmail.com>
2025-09-30 01:47:45 +0200

    Creating Recipe model with SQLAlchemy

---
+++ models.py
@@ -0,0 +1,10 @@
+from db_config import db
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Recipe {self.title}>'
